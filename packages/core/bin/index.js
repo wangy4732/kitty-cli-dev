c2ca45d1d830989be1ae68dde39cbb46fa70e460
wyw <wywjobnumber@gmail.com>
2021-07-30 23:52:20 +0800

    add cli commond

---
+++ packages/core/bin/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-30 23:48:16
+ * @LastEditTime: 2021-07-30 23:49:19
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /test-cli-dev/packages/core/bin/index.js
+ */
+#! /usr/bin//env node
+
+console.log('hello kitty-cli-dev')
